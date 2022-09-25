0c33cd7883ae4eb92b875b5b83179b49db09e914
William Marshburn <marshburndev@gmail.com>
2022-09-24 20:15:48 -0400

    Add files via upload

---
+++ services/services/AppointmentService.java
@@ -0,0 +1,24 @@
+package services;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Date;
+
+public class AppointmentService {
+	public Map<String, Appointment> appointments = new HashMap<>();//Creating HashMap to hold Appointment objects. 
+	int appointmentId = 0;//Is incremented in addApointment method to give a unique ID. 
+	
+	//Add appointment method that constructs an appointment object.  
+	public void addAppointment(Date appointmentDate, String description) {
+		appointmentId++;
+		String newID = Integer.toString(appointmentId);
+		Appointment newAppt = new Appointment(newID, appointmentDate, description);
+		appointments.put(newAppt.getAppointmentId(), newAppt);
+	}
+	
+	//Deletes appointment object. 
+	public void deleteAppointment(String appointmentId) {
+		if(appointments.containsKey(appointmentId)) {
+			appointments.remove(appointmentId);
+		}
+	}
+}
---
+++ services/services/Task.java
@@ -0,0 +1,92 @@
+package services;
+
+public class Task {
+	private String uniqueTaskId; //no longer than 10 characters, cannot update, non null
+	private String name; //no longer than 20 characters, non null
+	private String description; //no longer than 50 characters, non null
+	
+    public boolean idValidation(String id){ //ID validation
+        if(id.isEmpty()){
+            return false;
+        }else if (id.length() > 10){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    public boolean nameValidation(String name){ //Name Validation
+        if(name.isEmpty()){
+            return false;
+        }else if (name.length() > 20){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    public boolean descriptionValidation(String description){ //Descritpion validation
+        if(description.isEmpty()){
+            return false;
+        }else if (description.length() > 50){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    //Getters
+
+	public String getUniqueTaskId() {
+		return this.uniqueTaskId;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+	
+	//Setters
+    
+	public void setName(String name) throws IllegalArgumentException {
+		if(this.nameValidation(name)) {
+			this.name = name;
+		}else {
+			throw new IllegalArgumentException("Unable to accept name, please enter a valid name.");
+		}
+	}
+	
+	public void setDescription(String description) throws IllegalArgumentException {
+		if(this.descriptionValidation(description)) {
+			this.description = description;
+		}else {
+			throw new IllegalArgumentException("Unable to accept description, please shorten the description text.");
+		}
+	}
+	
+	//Constructor
+	
+	public Task(String uniqueTaskId, String name, String description) throws IllegalArgumentException{
+		if(this.idValidation(uniqueTaskId)) {
+			this.uniqueTaskId = uniqueTaskId;
+		}else {
+			throw new IllegalArgumentException("Invalid ID, too long.");
+		}
+		
+        if(this.nameValidation(name)){
+            this.name = name;
+        }else{
+        	throw new IllegalArgumentException("Invalid first name, too long.");
+        }
+        
+		if(this.descriptionValidation(description)) {
+			this.description = description;
+		}else {
+			throw new IllegalArgumentException("Invalid description, too long.");
+		}
+	}
+}
+
